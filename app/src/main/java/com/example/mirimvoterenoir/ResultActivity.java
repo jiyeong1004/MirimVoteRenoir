5e470662f3a75788d3cd4a4eaa3eada0b1bf8cbf
jiyeong1004 <ak1157@naver.com>
2020-11-24 22:36:39 +0900

    Update ResultActivity

--- app/src/main/java/com/example/mirimvoterenoir/ResultActivity.java
+++ app/src/main/java/com/example/mirimvoterenoir/ResultActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
+    Integer imageFileId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +26,28 @@ public class ResultActivity extends AppCompatActivity {
 
         int[] textIds = {R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5, R.id.text6, R.id.text7, R.id.text8, R.id.text9};
         int[] ratingIds = {R.id.rating_bar1, R.id.rating_bar2, R.id.rating_bar3, R.id.rating_bar4, R.id.rating_bar5, R.id.rating_bar6, R.id.rating_bar7, R.id.rating_bar8, R.id.rating_bar9};
-        Integer imageFileId[] = {R.drawable.o1, R.drawable.o2, R.drawable.o3, R.drawable.o4, R.drawable.o5, R.drawable.o6, R.drawable.o7, R.drawable.o8, R.drawable.o9};
+        int[] imageFileId = {R.drawable.o1, R.drawable.o2, R.drawable.o3, R.drawable.o4, R.drawable.o5, R.drawable.o6, R.drawable.o7, R.drawable.o8, R.drawable.o9};
 
-        int max = 0;
-        int maxIndex = 0;
+        int max = 0, maxIndex = 0;
 
         for(int i = 0; i < imgNames.length; i++){
             if(voteCount[i] > max){
                 max = voteCount[i];
                 maxIndex = i;
             }
-        }
 
-        TextView textMaxTitle = findViewById(R.id.text_max_title);
-        ImageView imgvMax = findViewById(R.id.imgv_max);
-
-        textMaxTitle.setText(imgNames[maxIndex]);
-        imgvMax.setImageResource(imgFileNames[maxIndex]);
-
-        for(int i = 0; i < imgNames.length; i++){
             textVs[i] = findViewById(textIds[i]);
             ratingBars[i] = findViewById(ratingIds[i]);
 
             textVs[i].setText(imgNames[i]);
             ratingBars[i].setRating((float)voteCount[i]);
         }
+
+        TextView textMaxTitle = findViewById(R.id.text_max_title);
+        ImageView imgvMax = findViewById(R.id.imgv_max);
+
+        textMaxTitle.setText(imgNames[maxIndex]);
+        imgvMax.setImageResource(imageFileId[maxIndex]);
 
         Button btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
